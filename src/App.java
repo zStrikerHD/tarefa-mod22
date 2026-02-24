e65436d4d1ec11ead6989b13000a9ee4e7e26f05
Giovani <giovani@teste.com>
2026-02-24 14:34:33 -0300

    tarefa

---
+++ src/App.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class App {
+    static void main(String[] args) {
+        dadosPessoas();
+    }
+
+    private static void dadosPessoas() {
+        Scanner sc = new Scanner(System.in);
+        Pessoa pessoa;
+        List<Pessoa> listaPessoas = new ArrayList<>();
+        int opcao = 0;
+
+        do {
+            System.out.println("## Escolha uma das opções abaixo ##");
+            System.out.println("Opção 1 - Cadastra pessoas");
+            System.out.println("Opção 2 - Imprime pessoas cadastradas");
+            System.out.println("Opção 3 - Imprime pessoas femininas");
+            System.out.println("Opção 0 - Sair do programa");
+            System.out.println("_______________________");
+
+            System.out.print("Digite aqui sua opção: ");
+            opcao = Integer.parseInt(sc.nextLine());
+
+            if(opcao == 1){
+                pessoa = new Pessoa();
+
+                System.out.print("Digite o código: ");
+                pessoa.setCodigo(Integer.parseInt(sc.nextLine()));
+
+                System.out.print("Digite o nome: ");
+                pessoa.setNome(sc.nextLine());
+
+                System.out.print("Digite o genero: ");
+                pessoa.setGenero(sc.nextLine());
+
+                System.out.println();
+
+                listaPessoas.add(pessoa);
+            }else if(opcao == 2){
+                if(listaPessoas.isEmpty()){
+                    System.out.println("Não existem pessoas cadastradas, pressione uma tecla para continuar!");
+                    sc.nextLine();
+                }else{
+                    System.out.println(listaPessoas.toString());
+
+                    System.out.println("Pressione um tecla para continuar.");
+                    sc.nextLine();
+                }         
+            } else if (opcao == 3) {
+                if (listaPessoas.isEmpty()){
+                    System.out.println("Não existem pessoas cadastradas, pressione uma tecla para continuar!");
+                    sc.nextLine();
+                }else {
+                    Stream<Pessoa> stream = listaPessoas.stream()
+                            .filter(pessoa1 -> pessoa1.getGenero().equalsIgnoreCase("Feminino"));
+                    stream.forEach(p -> System.out.println(p.getNome()));
+                }
+            }
+        } while (opcao != 0);
+
+        sc.close();
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,40 @@
+public class Pessoa {
+
+    private String nome;
+
+    private String genero;
+
+    private Integer codigo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
+}
